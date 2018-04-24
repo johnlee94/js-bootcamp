779e9da2392012e6611c164e983856d298873762
john_lee <jjlee099@gmail.com>
2018-04-23 23:18:09 -0700

    strings

--- basics/strings.js
+++ basics/strings.js
@@ -6,3 +6,12 @@ let fullName = `${firstName} ${lastName}`;
 console.log(fullName);
 
 let camelCase = 'camel case!'
+
+// Challenge
+// return city, country, location using String Concatenation
+
+let city = 'Sex Town';
+let country = 'Blowjob Island';
+let location = 'mimosas?'
+
+console.log(`${city}, ${country}, ${location}`)
